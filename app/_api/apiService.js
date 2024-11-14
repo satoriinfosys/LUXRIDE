984515a0878b255945d9559c1bf38f382763998c
Madhav Gautam <madhav.g@homeloanexperts.com.au>
2024-11-14 08:01:35 +0545

    .env updated

--- app/_api/apiService.js
+++ app/_api/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "https://parash-limo-backend.onrender.com/api"; // Replace this with your API base URL
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8000/api"; // Replace this with your API base URL
 
 const apiService = axios.create({
   baseURL: BASE_URL,
